3685a5cdff18fd0e6a205e3f287311cc5b33d13c
Jeremi Adams <jeremiadamss@gmail.com>
2022-08-25 00:40:50 +0100

    API (backend & frontend call)

--- src/Context.js
+++ src/Context.js
@@ -5,6 +5,17 @@ const Context = React.createContext()
 
 function ContextProvider (props) {
 
+    useEffect(() => {
+        const options = {
+            methos: 'GET',
+            url: 'http://localhost:8080/shoes'
+        }
+
+        axios.request(options).then((response) => {
+            console.log(response.data)
+        })
+    }, [])
+
     return (
         <Context.Provider value={''}>
             {props.children}
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,7 +5,7 @@ import './Categories.scss'
 import ApparelImg from '../images/apparel-img.png'
 import ShoesImg from '../images/shoes-img.png'
 import JewelryImg from '../images/jewelry-img.png'
-import FurnitureImg from '../images/furniture-img.png' 
+import CosmeticsImg from '../images/cosmetics-img.png' 
 
 function Categories() {
 
@@ -23,17 +23,17 @@ function Categories() {
                 <div className="categories__category categories__category-shoes">
                     <img className="categories__category-img" src={ShoesImg} alt="Image of Apparel"/>
                     <h3 className="categories__category-heading">Shoes</h3>
-                    <p className="categories__category-text">Women's wear</p>
+                    <p className="categories__category-text">Women's Footwear</p>
                 </div>
                 <div className="categories__category categories__category-jewelry">
                     <img className="categories__category-img" src={JewelryImg} alt="Image of Apparel"/>
                     <h3 className="categories__category-heading">Jewelry</h3>
                     <p className="categories__category-text">Popular</p>
                 </div>
-                <div className="categories__category categories__category-furniture">
-                    <img className="categories__category-img" src={FurnitureImg} alt="Image of Apparel"/>
-                    <h3 className="categories__category-heading">Furniture</h3>
-                    <p className="categories__category-text">Popular</p>
+                <div className="categories__category categories__category-cosmetics">
+                    <img className="categories__category-img" src={CosmeticsImg} alt="Image of Apparel"/>
+                    <h3 className="categories__category-heading">Cosmetics</h3>
+                    <p className="categories__category-text">Women's Makeup</p>
                 </div>
             </div>
         </section>
