8be04c4205cd59d842f95a6c4abf6189f2c6ec52
Jeremi Adams <jeremiadamss@gmail.com>
2023-01-16 03:43:11 +0100

    eslint deploy error changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 import './Footer.scss'
 import FooterLogo from '../images/footer-logo.png'
--- src/components/ShoesProductDetail.js
+++ src/components/ShoesProductDetail.js
@@ -1,5 +1,5 @@
-import React, {useContext, useRef} from 'react'
-import {useParams} from 'react-router-dom'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
 
 import './ProductDetail.scss'
 import {Context} from '../Context'
